f696ab5b90f297a9195e69474fe77ba2d633e6db
chouna <lucressechouna640@gmail.com>
2025-04-13 10:25:13 +0100

    mini caluclatrice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Calculatrice from './pages/Calculatrice'
 import Exemple from './pages/Exemple'
 
 function App() {
 
   return (
     <>
-    <Exemple/>
+  <Calculatrice/>
      
     </>
   )
